0b2473896d9d4dc4a86c14f04c563ca1b24e1d85
rahmathadiyatul <rahmathadiyatulakbar@gmail.com>
2025-08-02 23:25:24 +0700

    Env variable adjustment

--- src/app/controller/useMenuData.ts
+++ src/app/controller/useMenuData.ts
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react"
 import type { MenuCategory, MenuData } from "@/database/page"
 
-const NETLIFY_BASE_URL = process.env.NETLIFY_BASE_URL
+const NETLIFY_BASE_URL = process.env.NEXT_PUBLIC_NETLIFY_BASE_URL
 
 export default function useMenuData(isEditor = false) {
     const [menuCategory, setMenuCategory] = useState<MenuCategory[]>([])
